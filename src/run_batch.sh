63260fcec204fe408c3e7c94ce97e153d386ba8b
Lance Parrish <lanciferp@gmail.com>
2023-09-08 08:51:14 -0400

    Update run_batch.sh

--- src/run_batch.sh
+++ src/run_batch.sh
@@ -13,11 +13,10 @@ delete_finished=$5
 start_point=$6
 batch_size=$7
 
-arguments= "--source" "$1" "--weights" "$2" "--csv" "$3" "--append" "--finished" "$4"
+arguments="--source" "$1" "--weights" "$2" "--csv" "$3" "--append" "--finished" "$4"
 
 if [$delete_finished]; then
-  $arguments+= "--delete_finished"
-  exit
+  $arguments+=" --delete_finished"
 fi
 
 if [start_point]; then
