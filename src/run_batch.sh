91c81c4f66d93699b223715b56b888544bc0489f
Lance Parrish <lanciferp@gmail.com>
2023-09-25 11:32:00 -0400

    Update run_batch.sh

--- src/run_batch.sh
+++ src/run_batch.sh
@@ -6,7 +6,7 @@
 #SBATCH -e ./flor_out/%j-err.txt
 #SBATCH -o ./flor_out/%j-out.txt
 #SBATCH -p htc
-sudo export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -qq ffmpeg libsm6 libxext6  -y < /dev/null > /dev/null
 source /shared/home/cyclemgmt/FlorHTR_env/bin/activate
 
