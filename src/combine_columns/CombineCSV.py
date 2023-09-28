3e79edcbad7d50fba959ac876cc5cd501ebef2dd
Lance Parrish <lanciferp@gmail.com>
2023-09-27 22:07:42 -0400

    added utah

---
+++ src/combine_columns/CombineCSV.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import argparse
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--job_config', type=str, required=True)
+    parser.add_argument('--')
+
+
+
+
