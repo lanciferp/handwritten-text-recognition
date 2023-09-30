b7dfc24f03b12c05b1071500d824728b2e8dc25f
Lance Parrish <lanciferp@gmail.com>
2023-09-30 10:22:43 -0400

    fixed paths

--- src/combine_columns/run_age_months.sh
+++ src/combine_columns/run_age_months.sh
@@ -10,4 +10,4 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -qq ffmpeg libsm6 libxext6  -y < /dev/null > /dev/null
 source /shared/home/cyclemgmt/FlorHTR_env/bin/activate
 
-python3 -u ./AgeMonths.py "--year" $1 "--month" $2 "--relation" $3 "--output" $4
+python3 -u ./combine_columns/AgeMonths.py "--year" $1 "--month" $2 "--relation" $3 "--output" $4
