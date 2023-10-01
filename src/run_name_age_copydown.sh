cb287026a0ae94e040fb23c0d39b36fed92f8643
Lance Parrish <lanciferp@gmail.com>
2023-09-30 22:48:09 -0400

    updated memory requirements

--- src/run_name_age_copydown.sh
+++ src/run_name_age_copydown.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --mem=100G   # memory per CPU core
-#SBATCH -J "NA"   # job name
+#SBATCH --mem=60G   # memory per CPU core
+#SBATCH -J "NA Cdown"   # job name
 #SBATCH -e ./flor_out/%j-err.txt
 #SBATCH -o ./flor_out/%j-out.txt
 #SBATCH -p hpc
+
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -qq ffmpeg libsm6 libxext6  -y < /dev/null > /dev/null
 source /shared/home/cyclemgmt/FlorHTR_env/bin/activate
