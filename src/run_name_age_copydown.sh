6582be24ce0e7b82a8d1035315c5cacac49baa23
Lance Parrish <lanciferp@gmail.com>
2023-09-30 23:11:42 -0400

    small changes

--- src/run_name_age_copydown.sh
+++ src/run_name_age_copydown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --mem=60G   # memory per CPU core
-#SBATCH -J "NA Cdown"   # job name
+#SBATCH -J "NA CDown"   # job name
 #SBATCH -e ./flor_out/%j-err.txt
 #SBATCH -o ./flor_out/%j-out.txt
 #SBATCH -p hpc
